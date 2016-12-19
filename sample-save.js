45f641d4e4841a0defff415a43467fc2daee5d15
camdeardorff <cameron.deardorff@yahoo.com>
2016-12-19 04:07:09 +0000

    restructuring persistance setup

---
+++ sample-save.js
@@ -0,0 +1,56 @@
+var jsonfile = require('jsonfile');
+const HOLD_FILE = '/home/root/CafSense/persistence/saved-samples.json'
+
+
+/*
+Function: Save Failed Sample
+Purpose: saves away a sample that was not able to be sent to the server for whatever reason.
+Params: sample: {obj}
+*/
+module.exports = {
+
+	save: function (sample, callback) {
+		console.log("save sample: ", sample);
+		jsonfile.readFile(HOLD_FILE, function (err, savedData) {
+			if (!err) {
+				if (!savedData) {
+					savedData = {
+						samples: []
+					}
+				} else if (!savedData.samples) {
+					savedData.samples = [];
+				}
+				savedData.samples.push(sample);
+				jsonfile.writeFile(HOLD_FILE, savedData, (err) => {
+					callback(err);
+				});
+			} else {
+				callback(err);
+			}
+		});
+	},
+
+
+	removeAll: function (callback) {
+		console.log("remove all samples");
+		var savedData = {
+			samples: []
+		};
+		jsonfile.writeFile(HOLD_FILE, savedData, (err) => {
+			callback(err);
+		});
+	},
+
+
+	getAll: function (callback) {
+		console.log("get all saved samples");
+		jsonfile.readFile(HOLD_FILE, (err, savedData) => {
+			if (savedData) {
+				var samples = savedData.samples;
+				callback(err, savedData.samples);
+			} else {
+				callback(err);
+			}
+		});
+	}
+};
